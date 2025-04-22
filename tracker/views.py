0748a28a4ff16c2a91aa05b71ccf17249dd8be3a
andrewdaekim99 <ankim981693@gmail.com>
2025-04-21 21:30:26 -0700

    Created register view in views.py to handle user registrations.

--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
 
-# Create your views here.
+def register_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
+
+        # 1. Check if passwords match
+        if password != confirm_password:
+            messages.error(request, "Passwords do not match")
+            return render(request, 'tracker/register.html')
+        
+        # 2. Check if username already exists
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Username already exists.")
+            return render(request, 'tracker/register.html')
+        
+        # 3. Create the user
+        user = User.objects.create_user(username=username, password=password)
+        user.save()
+
+        messages.success(request, "Account created! You can now log in.")
+        return redirect('login') # use the name of login URL pattern
+    
+    return render(request, 'tracker/register.html')
